0c142bd3f2e1ec41cd58cd469a41a221c6e33f17
Iver369 <ibna01101001@gmail.com>
2025-11-06 15:06:33 +0100

    fikset kode

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  while(!Serial);
   eksempelfunksjon();
 }
 
@@ -14,6 +15,4 @@ void loop() {
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+
