a70b36dc6411b1bb5254cf1d692dce03ee1d8d3b
Iver369 <ibna01101001@gmail.com>
2025-11-06 14:00:21 +0100

    eksempel.h og eksempel.cpp

---
+++ include/eksempel.h
@@ -0,0 +1,6 @@
+#ifndef EKSEMPEL_H
+#define EKSEMPEL_H
+
+void eksempelFunksjon();
+
+#endif 
---
+++ src/eksempel.cpp
@@ -0,0 +1,6 @@
+#include "eksempel.h"
+#include <Arduino.h>
+
+void eksempelfunksjon() {
+  Serial.println("Hei fra eksempel.cpp!");
+}
