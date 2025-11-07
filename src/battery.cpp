a943502ca67849e51c8ac88326bd756b0e941f0a
ArneTorje <arnetoi@stud.ntnu.no>
2025-11-07 11:55:23 +0100

    battery

--- src/battery.cpp
+++ src/battery.cpp
@@ -1,9 +1,7 @@
 #include <Arduino.h>
 
 int battery_cap=100;
-int speed = 4; //må hentes inn i main kode
 int power = 0.5; // kan justeres for hvor fort batteriet skal tømmes
-int time = 1; // kan kjøre koden hvert sekund eller så må vi telle tiden bilen kjører
 int battery_calculator(int speed,int time){
     battery_cap -= abs((speed/time)*power); //enkel formel for å beregne hvor mye det blir igjen på batteriet
     if(battery_cap<=0){
