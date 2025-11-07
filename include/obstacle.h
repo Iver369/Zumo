ed78514ca9fd008997488017f0931e448c8121e9
Iver369 <ibna01101001@gmail.com>
2025-11-07 11:42:15 +0100

    ultrasonic

--- include/obstacle.h
+++ include/obstacle.h
@@ -1,4 +1,27 @@
 #ifndef OBSTACLE_H
 #define OBSTACLE_H
 
+class UltraSonicSensor {
+public:
+    void init(int trigPin, int echoPin, float threshold);
+    void readDistance();
+    bool isObstacleNear();
+    void averageDistance();
+    void printDebug();
+private:
+    long time();
+    // Constants
+    const float speedOfSound = 0.0343; // cm/µs
+    static const int numReadings = 5;
+    // Configuration Data
+    int trigPin;
+    int echoPin;
+    int threshold;
+    // Measurement Data
+    float distance;
+    float readings[numReadings];
+    int readIndex;
+    int total;
+    float average;
+};
 #endif
--- src/obstacle.cpp
+++ src/obstacle.cpp
@@ -1,1 +1,28 @@
 #include "obstacle.h"
+#include <Arduino.h>  // for pinMode, digitalWrite, pulseIn, Serial
+
+void UltraSonicSensor::init(int trigPin, int echoPin, float threshold) {
+    // body here
+}
+
+void UltraSonicSensor::readDistance() {
+    // body here
+}
+
+void UltraSonicSensor::averageDistance() {
+    // body here
+}
+
+void UltraSonicSensor::printDebug() {
+    // body here
+}
+
+long UltraSonicSensor::time() {
+    // body here
+}
+
+bool UltraSonicSensor::isObstacleNear() {
+    // body here
+    return false;
+}
+
