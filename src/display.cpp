524251a2db8d5316a401b0161ef0f680486244ed
miarosby <miaor@stud.ntnu.no>
2025-11-07 11:12:43 +0100

    2

--- src/display.cpp
+++ src/display.cpp
@@ -7,7 +7,11 @@ void display(){
     lcd.print("Batteri");
     lcd.gotoXY(0,1);
     lcd.print("-status:");
-    delay(5000);
+    delay(1000);
+    lcd.gotoXY(0,0);
+    lcd.print("x meter");
+    lcd.gotoXY(1,0);
+    lcd.print("igjen");
     lcd.clear();
 }
 
