40625e34a064a730a3ec2e400e04061564aac649
Vetle Husby <vetleaghusby@dhcp-10-22-97-62.wlan.ntnu.no>
2025-11-07 09:26:36 +0100

    Hjelper arne med kodefeil

--- src/battery.cpp
+++ src/battery.cpp
@@ -6,7 +6,7 @@ int strøm = 0.5; // kan justeres for hvor fort batteriet skal tømmes
 int time = 1; // kan kjøre koden hvert sekund eller så må vi telle tiden bilen kjører
 void battery_calculator(){
     batteri_cap= batteri_cap-((speed/time)*strøm); //enkel formel for å beregne hvor mye det blir igjen på batteriet
-    if(batteri_cap=<0){
+    if(batteri_cap<=0){
         Serial.println("Du er tom for strøm og bilen stopper");
     }
     else if (batteri_cap>15){
