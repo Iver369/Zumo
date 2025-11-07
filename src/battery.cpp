47048df01a11deec421d6af594faabeff280eefb
ArneTorje <arnetoi@stud.ntnu.no>
2025-11-07 10:00:51 +0100

    battery

--- src/battery.cpp
+++ src/battery.cpp
@@ -1,19 +1,20 @@
 #include <Arduino.h>
 
-int batteri_cap=100;
+int battery_cap=100;
 int speed = 4; //må hentes inn i main kode
-int strøm = 0.5; // kan justeres for hvor fort batteriet skal tømmes
+int power = 0.5; // kan justeres for hvor fort batteriet skal tømmes
 int time = 1; // kan kjøre koden hvert sekund eller så må vi telle tiden bilen kjører
-void battery_calculator(){
-    batteri_cap= batteri_cap-((speed/time)*strøm); //enkel formel for å beregne hvor mye det blir igjen på batteriet
-    if(batteri_cap<=0){
+int battery_calculator(int speed,int time){
+    battery_cap -= abs((speed/time)*power); //enkel formel for å beregne hvor mye det blir igjen på batteriet
+    if(battery_cap<=0){
         Serial.println("Du er tom for strøm og bilen stopper");
     }
-    else if (batteri_cap>15){
+    else if (battery_cap>15){
         Serial.println("Du er nesten tom for strøm og bør lade");
     }
     else{
         Serial.println("Dette er din batteri prosent:");
-        Serial.println(batteri_cap);
+        Serial.println(battery_cap);
     }
+    return battery_cap;
 }
