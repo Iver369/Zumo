2099d84e715c838846ef1966b10a3779c9f81182
miarosby <miaor@stud.ntnu.no>
2025-11-13 13:38:10 +0100

    nytt til display

--- src/display.cpp
+++ src/display.cpp
@@ -1,58 +1,58 @@
 #include <display.h>
 
 void displayStartup() {
-    lcd.clear();
-    lcd.gotoXY(0,0);
-    lcd.print("Kalibrert");
-    delay(1500);
-    lcd.clear();
-    lcd.gotoXY(0,0);
-    lcd.print("Batteri:");
-    lcd.gotoXY(0,1);
-    lcd.print(battery_cap);
-    lcd.gotoXY(7,1);
-    lcd.print("%");
-    delay(1500);
-    lcd.clear();
-    lcd.gotoXY(0,0);
-    lcd.print("Klar");
-    lcd.gotoXY(0,1);
-    lcd.print("til å");
-    delay(500);
-    lcd.gotoXY(0,0);
-    lcd.print("KJØRE!!");
-    delay(500);
-    lcd.clear();
-}
+    static unsigned long previousMillis = 0;
+    static int state = 0; 
+    unsigned long currentMillis = millis();
 
-void displayStatus() {
-    static unsigned long lastUpdate = 0;   
-    unsigned long now = millis();        
-
-
-    if (now - lastUpdate >= 1000) {
-        lastUpdate = now;
-
+switch (state) {
+    case 0:
         lcd.clear();
-        lcd.gotoXY(0, 0);
-        lcd.print("⚡︎: ");
-        lcd.gotoXY(0,1);
-        lcd.print(battery_cap);
-        lcd.gotoXY(8,1);
-        lcd.print("%");
-
-        lcd.gotoXY(0, 0);
-        lcd.print("Avstand:");
-        lcd.gotoXY(0,1);
-        lcd.print(remaining_distance());
-        lcd.gotoXY(7,1);
-        lcd.print("m");
+        lcd.gotoXY(0,0);
+        lcd.print("Kalibrert");
+        previousMillis = currentMillis;
+        state = 1;
+        break; 
+    
+    case 1:  
+        if (currentMillis - previousMillis >= 1500) {
+            lcd.clear();
+            lcd.gotoXY(0,0);
+            lcd.print("Batteri:");
+            lcd.gotoXY(0,1);
+            lcd.print(battery_cap);
+            lcd.gotoXY(7,1);
+            lcd.print("%");
+            previousMillis = currentMillis; 
+            state = 2; 
+        }
+        break; 
+    case 2:
+        if (currentMillis - previousMillis >= 1500) {
+            lcd.clear();
+            lcd.gotoXY(0,0);
+            lcd.print("Klar");
+            lcd.gotoXY(0,1);
+            lcd.print("til å");
+            previousMillis = currentMillis;
+            state = 3
+        }
+        break;
+   
+    case 3:
+        if (currentMillis - previousMillis >= 500) {
+            lcd.gotoXY(0,0);
+            lcd.print("KJØRE!");
+            previousMillis = currentMillis;
+            state = 4;
+        }
+        break = 4;
+    
+    case 4:
+        if (currentMillis - previousMillis >= 500) {
+            lcd.clear();
+            state = 5;
+        }
+        break; 
     }
 }
-
-// displayStartup if battery is low/empty, to go charge the battery
-// displayStatus if battery is low/empty, to go charge the battery
-// displayStatus obstacle distance/collision warning
-// displayStatus speed
-// displayStatus line sensor readings?
-// Or create separate functions for them
