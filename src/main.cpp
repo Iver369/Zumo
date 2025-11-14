fd8dfb2408fd34aaeb1d5fed6ca528db687a9696
Iver369 <ibna01101001@gmail.com>
2025-11-14 08:57:51 +0100

    main

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ UltraSonicSensor sensor;
 
 void setup() {
   Serial.begin(9600); // Initialize Serial communication
-  // sensor.init(12, 11, 20.0); // Example pins and threshold
+  sensor.init(12, 11, 20.0); // Example pins and threshold
   // Initialize shared line sensor object and motors (defined in globals.cpp)
   lineSensors.initFiveSensors(); // Initialize all five line sensors
   calibrateLineSensors(lineSensors, motors, 5000); // Calibrate for 5 secondss
@@ -18,21 +18,21 @@ void setup() {
 }
 
 void loop() {
-  // displayStatus();
-  /*static unsigned long lastUpdate = 0;
+  displayStatus();
+  static unsigned long lastUpdate = 0;
   unsigned long now = millis();
   float deltaTime = (now - lastUpdate) / 1000.0; // seconds
   lastUpdate = now;
   battery_calculator(deltaTime); 
-  checkBatteryState(); */
+  checkBatteryState(); 
 
-  // sensor.readDistance();
-  // sensor.averageDistance();
-  // sensor.printDebug();
-  /*if (sensor.isObstacleNear()) {
+  sensor.readDistance();
+  sensor.averageDistance();
+  sensor.printDebug();
+  if (sensor.isObstacleNear()) {
     Serial.println("Obstacle detected! Stopping motors.");
     motors.setSpeeds(0, 0); // Stop motors if obstacle is near
-  } else */
+  } else 
 
   linefollow();
   Serial.print("Offset: ");
