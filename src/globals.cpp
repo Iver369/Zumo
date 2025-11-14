0fcee1de0c2f1e6421540bb255579fdced905ff3
Iver369 <ibna01101001@gmail.com>
2025-11-14 10:35:00 +0100

    1

--- src/globals.cpp
+++ src/globals.cpp
@@ -12,4 +12,6 @@ Zumo32U4LCD lcd;
 float battery_cap=100;
 const float power=0.05; // kan justeres for hvor fort batteriet skal tømmes
 uint16_t threshold = 700;
+bool isCharging = false;
+unsigned long chargeStartTime = 0;
 int baseSpeed = 100;
